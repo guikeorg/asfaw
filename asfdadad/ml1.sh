4ad84ce71205cdfbfe30d6dc7a0d45b2c9c4e054
guikeorg <96020227+guikeorg@users.noreply.github.com>
2022-01-02 20:07:41 +0800

    Add files via upload

---
+++ asfdadad/ml1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
+rm -f  /etc/XrayR/config.yml &&  wget -O /etc/XrayR/config.yml https://raw.githubusercontent.com/guikeorg/asfaw/main/ml1.yml
+xrayr stop
+xrayr start
