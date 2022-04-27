e159e6bac84374a819a3173572670bf256c88826
guikeorg <96020227+guikeorg@users.noreply.github.com>
2022-04-27 16:55:48 +0800

    Update ml2.sh

--- asfdadad/ml2.sh
+++ asfdadad/ml2.sh
@@ -2,7 +2,7 @@
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
-bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
+bash <(curl -Ls https://raw.githubusercontents.com/Misaka-blog/XrayR-script/master/install.sh)
 rm -f  /etc/XrayR/config.yml &&  wget -O /etc/XrayR/config.yml https://raw.githubusercontent.com/guikeorg/asfaw/main/config.yml
 xrayr stop
 xrayr start
